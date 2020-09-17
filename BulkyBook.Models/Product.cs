ea7a1bb23541630b776dea045e8b4a059e16f04c
Muhammad Harith <harith781@gmail.com>
2020-09-17 14:41:22 +0800

    update validation for product model

--- BulkyBook.Models/Product.cs
+++ BulkyBook.Models/Product.cs
@@ -11,12 +11,16 @@ namespace BulkyBook.Models
         [Key]
         public int Id { get; set; }
 
+        [Required]
         public string Title { get; set; }
 
+        [Required]
         public string Description { get; set; }
 
+        [Required]
         public string ISBN { get; set; }
 
+        [Required]
         public string Author { get; set; }
 
         [Required]
