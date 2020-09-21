ebd6950a618f0ff663ec86ae7ca1b182a8ceba5e
Muhammad Harith <harith781@gmail.com>
2020-09-22 00:15:11 +0800

    added more properties to identity table

---
+++ BulkyBook.Models/ApplicationUser.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace BulkyBook.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+        [Required]
+        public string Name { get; set; }
+
+        public string StreetAddress { get; set; }
+
+        public string City { get; set; }
+
+        public string State { get; set; }
+
+        public string PostalCode { get; set; }
+
+        [NotMapped] //means these properties will not be pushed to the database
+        public string Role { get; set; }
+    }
+}
