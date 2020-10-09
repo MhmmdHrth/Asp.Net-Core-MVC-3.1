692a0b5a913594c68cb70b70116076bb06dd5d94
Muhammad Harith <harith781@gmail.com>
2020-10-10 02:08:17 +0800

    adding order summary controller and view

--- BulkyBook/Areas/Customer/Controllers/CartController.cs
+++ BulkyBook/Areas/Customer/Controllers/CartController.cs
@@ -123,5 +123,39 @@ namespace BulkyBook.Areas.Customer.Controllers
 
             return RedirectToAction(nameof(Index));
         }
+        
+        public IActionResult Summary()
+        {
+            var claimsIdentity = (ClaimsIdentity)User.Identity;
+            var claim = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier);
+
+            ShoppingCartVM = new ShoppingCartVM()
+            {
+                ListCart = _unitOfWork.ShoppingCart.GetAll(x => x.ApplicationUserId == claim.Value
+                                                            , includeProperties: "Product"),
+                OrderHeader = new Models.OrderHeader()
+            };
+
+            ShoppingCartVM.OrderHeader.ApplicationUser = _unitOfWork.ApplicationUser
+                                                .GetFirstOrDefault(x => x.Id == claim.Value, includeProperties: "Company");
+
+            foreach(var list in ShoppingCartVM.ListCart)
+            {
+                list.Price = SD.GetPriceBasedOnQuantity
+                                (list.Count, list.Product.Price, list.Product.Price50, list.Product.Price100);
+
+                ShoppingCartVM.OrderHeader.OrderTotal += (list.Price * list.Count);
+            }
+
+            //passing data for user details, either user want the details itself or other
+            ShoppingCartVM.OrderHeader.Name = ShoppingCartVM.OrderHeader.ApplicationUser.Name;
+            ShoppingCartVM.OrderHeader.PhoneNumber = ShoppingCartVM.OrderHeader.ApplicationUser.PhoneNumber;
+            ShoppingCartVM.OrderHeader.StreetAddress = ShoppingCartVM.OrderHeader.ApplicationUser.StreetAddress;
+            ShoppingCartVM.OrderHeader.City = ShoppingCartVM.OrderHeader.ApplicationUser.City;
+            ShoppingCartVM.OrderHeader.State = ShoppingCartVM.OrderHeader.ApplicationUser.State;
+            ShoppingCartVM.OrderHeader.PostalCode = ShoppingCartVM.OrderHeader.ApplicationUser.PostalCode;
+
+            return View(ShoppingCartVM);
+        }
     }
 }
