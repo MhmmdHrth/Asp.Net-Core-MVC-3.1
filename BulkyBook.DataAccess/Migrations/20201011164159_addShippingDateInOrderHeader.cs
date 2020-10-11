f9e75323be1f0c8fa834e9124fd9cfc73897c034
Muhammad Harith <harith781@gmail.com>
2020-10-12 01:04:52 +0800

    add missing propertis

---
+++ BulkyBook.DataAccess/Migrations/20201011164159_addShippingDateInOrderHeader.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BulkyBook.DataAccess.Migrations
+{
+    public partial class addShippingDateInOrderHeader : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "ShippingDate",
+                table: "OrderHeaders",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ShippingDate",
+                table: "OrderHeaders");
+        }
+    }
+}
